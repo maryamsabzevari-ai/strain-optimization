529cbb14cce149f67e605690dfb75e0af77dd888
maryamsabzevari-ai <85158868+maryamsabzevari-ai@users.noreply.github.com>
2021-06-09 10:11:27 +0300

    Update callrun.sh

--- callrun.sh
+++ callrun.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem=8G                                                                
 
 
-#module load anaconda2
+
 #mkdir /tmp/testname                                                            
 
 declare -a arr2=(0 1 2 3 4 5 6 7 8  )
